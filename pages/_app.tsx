45ead10fe47ab040963ddcf9b685eafd495b50b4
SianTharit <Tharit.preecha1@gmail.com>
2023-05-09 11:12:13 +0700

    Modal-Signup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Nunito } from 'next/font/google';
+import Head from 'next/head';
+import Navbar from '../src/components/navbar/Navbar';
+import ClientOnly from '../src/components/ClientOnly';
+import RegisterModal from '../src/components/modals/RegisterModal';
+import ToasterProvider from '../src/providers/ToasterProvider';
+
+const font = Nunito({
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={font.className}>
+      <Head>
+        <title>Airbnb</title>
+        <meta name="description" content="Airbnb clone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ClientOnly>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+      </ClientOnly>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
